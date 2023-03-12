122bf2a00eaf890b07e7b129a94cc24c6cf06a7a
Zh-Cancan <niconideyo@163.com>
2023-03-12 20:07:26 +0800

    实现cd命令（./../绝对路径/相对路径）

--- Shell/builtin.c
+++ Shell/builtin.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include "builtin.h"
+
+#define PATH_MAX 256
+#define EXIT_FAILURE -1
 
 int builtin_cd(char **des)
 {
@@ -14,10 +18,29 @@ int builtin_cd(char **des)
 		return 1;
 	}
 
-	// 完成cd命令
-	chdir(des[1]);
+    if (des[1][0] != '/') {
+		char cwd[PATH_MAX], path[PATH_MAX];
+
+		// 获取当前工作目录
+		if (getcwd(cwd, sizeof(cwd)) == NULL) {
+			perror("getcwd");
+			exit(EXIT_FAILURE);
+		}
+		// 拼接路径
+		if (sprintf(path, "%s/%s", cwd, des[1]) < 0) {
+			perror("sprintf");
+			exit(EXIT_FAILURE);
+		}
+		chdir(path);
+		getcwd(cwd, sizeof(cwd));
+		// 获取绝对路径的目录部分
+		setGlobalVLByName("PWD", cwd);
+    }
+	else {
+		chdir(des[1]);     // 完成cd命令
+		setGlobalVLByName("PWD", des[1]);
+	}
 
 	// 更改pwd
 	//setGlobalVLByName("OLDPWD", getVLByName("PWD"));
-	setGlobalVLByName("PWD", des[1]);
 }
